5aa89560e143a912080f4885a839b18b50ccfa58
Frempong <samankwaa.frempong@gmail.com>
2023-04-11 18:05:02 +0000

    Create Database vet_clinic

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+
+
+
+
+
+
+
+-- CREATE TABLE animals (
+--     name varchar(100)
+-- );
