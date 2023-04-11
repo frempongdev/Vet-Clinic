5ba21ef97d5022ef9335dc6e9804d4903968a19c
Frempong <samankwaa.frempong@gmail.com>
2023-04-11 18:09:30 +0000

    Create Table animals with all columns

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 CREATE DATABASE vet_clinic;
 
+ CREATE TABLE animals (id integer PRIMARY KEY, name char(50), date_of_birth date, escape_attempts int, neutered boolean, weight_kg decimal(10,2));
 
 
 
