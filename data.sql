ee82a1a91f930925e499a708ad4f5e4852f06a22
Frempong <samankwaa.frempong@gmail.com>
2023-04-14 18:29:08 +0000

    Insert data into specialization Table

--- data.sql
+++ data.sql
@@ -35,3 +35,6 @@ UPDATE animals SET owners_id = (SELECT id FROM owners WHERE full_name = 'Dean Wi
 
 
 INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, '2000-04-23'), ('Maisy Smith', 26, '2019-01-17'), ('Stephanie Mendez', 64, '1981-05-04'), ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO specialization (species_id, vets_id) VALUES ((SELECT id FROM species WHERE name='Pokemon'), (SELECT id FROM vets WHERE name='William Tatcher')), ((SELECT id FROM species WHERE name='Digimon'), (SELECT id FROM vets WHERE name='Stephanie Mendez')), ((SELECT id FROM species WHERE name='Pokemon'), (SELECT id FROM vets WHERE name='Stephanie Mendez')), ((SELECT id FROM species WHERE name='Digimon'), (SELECT id FROM vets WHERE name='Jack Harkness'));
+
