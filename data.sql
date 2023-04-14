78b8aba52d32f64df98efab54b67a90aed3881f3
Frempong <samankwaa.frempong@gmail.com>
2023-04-14 12:20:31 +0000

    Insert data into vets Table

--- data.sql
+++ data.sql
@@ -32,3 +32,6 @@ UPDATE animals SET owners_id = (SELECT id FROM owners WHERE full_name = 'Bob') W
 UPDATE animals SET owners_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name IN ('Charmander','Squirtle','Blossom');
 
 UPDATE animals SET owners_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name IN ('Angemon','Boarmon');
+
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, '2000-04-23'), ('Maisy Smith', 26, '2019-01-17'), ('Stephanie Mendez', 64, '1981-05-04'), ('Jack Harkness', 38, '2008-06-08');
